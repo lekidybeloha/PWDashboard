79be94694b07f574afbc61bb33b85b90cb3fbf53
Elkana Vinet <elkana.dimbiniaina@hotmail.com>
2019-02-19 22:26:10 +0300

    Return to laravel base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
                 </div>
             @endif
 
-            <div class="content">
+            <div class="content"> 
                 <div class="title m-b-md">
                     PWDashboard
                 </div>
